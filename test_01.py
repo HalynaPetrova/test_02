c6bbfd7bedbf71d0ac7a1e4b751914f687d204fd
Halyna Petrova <1208petrova@gmail.com>
2023-01-29 20:29:52 +0200

    ver_01

---
+++ test_01.py
@@ -0,0 +1,19 @@
+def print_hy(name):
+    print(name)
+
+
+print_hy('Kostya')
+
+
+def print_hy_1(name):
+    print(name)
+
+
+print_hy_1('Halyna')
+
+
+def print_hy_2(name):
+    print(name)
+
+
+print_hy_2('Leva')
